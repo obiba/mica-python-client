e288fa4122c3bd1248e77685c54d1049e2cc97cb
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2023-04-27 11:05:57 -0400

    updated import service

--- obiba_mica/import_zip.py
+++ obiba_mica/import_zip.py
@@ -7,6 +7,21 @@ import os.path
 import os
 
 class FileImportService:
+
+  def __init__(self, client: MicaClient, verbose: bool = False):
+     self.client = client
+     self.verbose = verbose
+
+  def __make_request(self):
+      request = self.client.new_request()
+      request.method('POST')
+      request.fail_on_error()
+      request.content_type('multipart/form-data')
+      request.accept_json()
+      if self.verbose:
+          request.verbose()
+      return request
+
   @classmethod
   def add_arguments(cls, parser):
       """
@@ -15,22 +30,15 @@ class FileImportService:
       parser.add_argument('path', help='Path to the zip file or directory that contains zip files to be imported')
       parser.add_argument('--publish', '-pub', action='store_true', help='Publish imported study')
 
-  @classmethod
-  def import_zip(cls, args, path):
+
+  def import_zip(self, path, publish: bool = None):
       """
       Import the Zip file content
       """
       print("Importing " + path + "...")
-      # Build and send request
-      request = MicaClient.build(MicaClient.LoginInfo.parse(args)).new_request()
-      request.fail_on_error()
 
-      if args.verbose:
-          request.verbose()
-
-      # send request
-      request.content_upload(path).accept_json().content_type('multipart/form-data')
-      response = request.post().resource('/draft/studies/_import?publish=' + str(args.publish).lower()).send()
+      query = "publish=%s" % str(publish).lower() if publish is not None and publish else ''
+      response = self.__make_request().content_upload(path).resource('/draft/studies/_import?%s' % query).send()
 
       # format response
       res = response.content
@@ -41,14 +49,16 @@ class FileImportService:
 
   @classmethod
   def do_command(cls, args):
+      service = FileImportService(MicaClient.build(MicaClient.LoginInfo.parse(args)), args.verbose)
+
       """
       Execute Import Zip command
       """
       if args.path.endswith('.zip'):
-          cls.import_zip(args, args.path)
+          service.import_zip(args.path, args.publish)
       else:
           for export_file in os.listdir(args.path):
               if export_file.endswith('.zip'):
-                  cls.import_zip(args, args.path + '/' + export_file)
+                  service.import_zip(args.path + '/' + export_file)
 
 
