a905cce8c8dccbe70237efb38688885100032367
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2025-12-10 13:59:59 -0500

    Write binary to stdout buffer

--- obiba_mica/rest.py
+++ obiba_mica/rest.py
@@ -103,13 +103,19 @@ class RestService:
         response = request.send()
 
         # format response
-        res = response.as_json()
-        if args.json:
-            res = response.pretty_json()
-        elif args.method in ['OPTIONS']:
-            res = response.headers['Allow']
+        if args.accept and 'json' not in args.accept.lower():
+            # Binary or non-JSON response - output raw content
+            if response.content:
+                sys.stdout.buffer.write(response.content)
+        else:
+            # JSON response - format and print
+            res = response.as_json()
+            if args.json:
+                res = response.pretty_json()
+            elif args.method in ['OPTIONS']:
+                res = response.headers['Allow']
 
-        # output to stdout
-        print(res)
+            # output to stdout
+            print(res)
     finally:
         client.close()
